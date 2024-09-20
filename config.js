f490214a38efd8631f282e6f87c22acdac770a25
Pathumkalhara111 <kalharapathum355@gmail.com>
2024-09-20 13:11:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "mS4AjLJS#6PJYW-rYsqRHiobF9NV58ZYotePBskMVlYxIctShd2I",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
